b5f56cffbf709c6055320bb4f00b68b674992439
Vitor <vitorcostadev@gmail.com>
2022-08-24 11:55:50 -0300

    updt secret-words

--- secret-word/src/App.js
+++ secret-word/src/App.js
@@ -20,7 +20,33 @@ const stages = [
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState();
+  const [letters, setLetters] = useState();
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words);
+    const category =
+      categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    //pick a random word
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+
+  };
   const startGame = () => {
+    // pick word and pick category
+    const {word,category} = pickWordAndCategory();
+    let wordLetters = word.split("");
+    wordLetters = wordLetters.map((l)=>l.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
   const verifyLetter = () => {
--- secret-word/src/components/Game.js
+++ secret-word/src/components/Game.js
@@ -2,11 +2,33 @@ import "./Game.css";
 
 const Game = ({ verifyLetter }) => {
   return (
-    <div>
-      <h1>Game</h1>
-      <button onClick={verifyLetter}>
-        Finalizar Jogo
-      </button>
+    <div className="games">
+      <p className="points">
+        <span>Pontuação: 0</span>
+      </p>
+      <h1>Adivinhe a palavra</h1>
+      <h3 className="tip">
+        Dica sobre a palavra:
+        <span> Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+    <div className="letterContainer">
+        <p>Tente adivinhar uma letra da palavra:</p>
+        <form>
+            <input type="text" name="letter" maxLength="1" required/>
+            <button>Jogar!</button>
+        </form>
+    </div>
+    <div className="wrongLettersContainer">
+        <p>Letras já utilizadas:</p>
+        <span>a,</span>
+        <span>b,</span>
+    </div>
+
+      <button onClick={verifyLetter}>Finalizar Jogo</button>
     </div>
   );
 };
