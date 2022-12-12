0c9e9130dd777f6432f2edca020c2d210217c8d3
Vitor <vitorcostadev@gmail.com>
2022-12-12 16:01:58 -0300

    new posts page

--- miniblog/src/pages/Home/index.js
+++ miniblog/src/pages/Home/index.js
@@ -1,12 +1,41 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 //CSS
-import styles from "./Home.module.css"
+import styles from "./Home.module.css";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  // components
+  const [query, setQuery] = useState("");
+  const [posts] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
 
-export default Home
+  return (
+    <div className={styles.home}>
+      <h1>Veja os nossos posts mais recentes</h1>
+      <form onSubmit={handleSubmit} className={styles.search_form}>
+        <input
+          type="text"
+          placeholder="Ou busque por tags..."
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button className="btn btn-dark">Pesquisar</button>
+      </form>
+      <div>
+        <h2>Posts...</h2>
+        {posts && posts.length === 0 && (
+          <div className={styles.noposts}>
+            <p>Não foram encontrados posts</p>
+            <Link to="/posts/create" className="btn">
+              Criar primeiro post
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
