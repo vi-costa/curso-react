bc7d5b0f075bb0182b5af8390e4396af763bf3ef
Vitor <vitorcostadev@gmail.com>
2023-03-01 16:04:57 -0300

    feat: backend final implementations

--- reactgram/backend/controllers/PhotoController.js
+++ reactgram/backend/controllers/PhotoController.js
@@ -79,14 +79,103 @@ const getUserPhotos = async (req, res) => {
 // Get photo by id
 const getPhotoById = async (req, res) => {
   const { id } = req.params;
-  const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+  const photo = await Photo.findById(mongoose.Types.ObjectId(id));
 
   // Check if photo exists
-  if(!photo) {
-    res.status(404).json({errors:["Foto não encontrada"]})
-    return
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
   }
-  res.status(200).json(photo)
+  res.status(200).json(photo);
+};
+
+// Update a photo
+const updatePhoto = async (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const reqUser = req.user;
+  const photo = await Photo.findById(id);
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
+  }
+
+  // Check if photo belongs to user
+  if (!photo.userId.equals(reqUser._id)) {
+    res.status(422).json({
+      errors: ["Ocorreu um erro, por favor tente novamente mais tarde."],
+    });
+    return;
+  }
+  if (title) {
+    photo.title = title;
+  }
+  await photo.save();
+  res.status(200).json({ photo, message: "Foto atualizada com sucesso!" });
+};
+
+// Like functionality
+const likePhoto = async (req, res) => {
+  const { id } = req.params;
+  const reqUser = req.user;
+  const photo = await Photo.findById(id);
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
+  }
+
+  // Check if user already liked the photo
+  if (photo.likes.includes(reqUser._id)) {
+    res.status(422).json({ errors: ["Você já curtiu a foto."] });
+    return;
+  }
+
+  // Put user id in likes array
+  photo.likes.push(reqUser._id);
+  photo.save();
+  res
+    .status(200)
+    .json({ photoId: id, userId: reqUser._id, message: "A foto foi curtida." });
+};
+
+// Comment functionality
+const commentPhoto = async (req, res) => {
+  const { id } = req.params;
+  const { comment } = req.body;
+  const reqUser = req.user;
+  const user = await User.findById(reqUser._id);
+  const photo = await Photo.findById(id);
+
+  // Check if photo exists
+  if (!photo) {
+    res.status(404).json({ errors: ["Foto não encontrada"] });
+    return;
+  }
+  // Put coment in the arra of comments
+  const userComment = {
+    comment,
+    userName: user.name,
+    userImage: user.profileImage,
+    userId: user._id,
+  };
+  photo.comments.push(userComment);
+  await photo.save();
+  res.status(200).json({
+    comment: userComment,
+    message: "O comentário foi adicionado com sucesso.",
+  });
+};
+
+// Search photos by title
+const searchPhotos = async (req, res) => {
+  const { q } = req.query;
+  const photos = await Photo.find({ title: new RegExp(q, "i") }).exec();
+  res.status(200).json(photos);
 };
 
 module.exports = {
@@ -94,5 +183,9 @@ module.exports = {
   deletePhoto,
   getAllPhotos,
   getUserPhotos,
-  getPhotoById
+  getPhotoById,
+  updatePhoto,
+  likePhoto,
+  commentPhoto,
+  searchPhotos
 };
--- reactgram/backend/middlewares/photoValidation.js
+++ reactgram/backend/middlewares/photoValidation.js
@@ -18,6 +18,24 @@ const photoInsertValidation = () => {
   ];
 };
 
-module.exports = { 
-    photoInsertValidation,
-}
+const photoUpdateValidation = () => {
+  return [
+    body("title")
+      .isString()
+      .withMessage("O título é obrigatório.")
+      .isLength({ min: 3 })
+      .withMessage("O título precisa ter no mínimo 3 caracteres."),
+  ];
+};
+
+const commentValidation = () => {
+  return [
+    body("comment").isString().withMessage("O comentário é obrigatório."),
+  ];
+};
+
+module.exports = {
+  photoInsertValidation,
+  photoUpdateValidation,
+  commentValidation,
+};
